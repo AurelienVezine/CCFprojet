dfb4b700fa685c02ed83359cea3b3ef50d92bb65
AurelienVezine <152594941+AurelienVezine@users.noreply.github.com>
2024-06-11 15:17:30 +0200

    Depart

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,11 +8,9 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
     }
 }
