b0e5864e190e6ba252809cabd1a28b88ca8927ab
AurelienVezine <152594941+AurelienVezine@users.noreply.github.com>
2024-06-12 00:19:58 +0200

    Debut du projet - creation des entity - relation entre entity

--- src/Entity/Animal.php
+++ src/Entity/Animal.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\AnimalRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: AnimalRepository::class)]
@@ -11,27 +13,42 @@ class Animal
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    private ?int $animal_id = null;
+    private ?int $id = null;
 
     #[ORM\Column(length: 50)]
-    private ?string $name = null;
+    private ?string $prenom = null;
 
     #[ORM\Column(length: 50)]
     private ?string $etat = null;
 
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Race $race = null;
+
+    /**
+     * @var Collection<int, RapportVeterinaire>
+     */
+    #[ORM\OneToMany(targetEntity: RapportVeterinaire::class, mappedBy: 'animal')]
+    private Collection $rapports;
+
+    public function __construct()
+    {
+        $this->rapports = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
-        return $this->animal_id;
+        return $this->id;
     }
 
-    public function getName(): ?string
+    public function getPrenom(): ?string
     {
-        return $this->name;
+        return $this->prenom;
     }
 
-    public function setName(string $name): static
+    public function setPrenom(string $prenom): static
     {
-        $this->name = $name;
+        $this->prenom = $prenom;
 
         return $this;
     }
@@ -44,6 +61,48 @@ class Animal
     public function setEtat(string $etat): static
     {
         $this->etat = $etat;
+
+        return $this;
+    }
+
+    public function getRace(): ?Race
+    {
+        return $this->race;
+    }
+
+    public function setRace(?Race $race): static
+    {
+        $this->race = $race;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, RapportVeterinaire>
+     */
+    public function getRapports(): Collection
+    {
+        return $this->rapports;
+    }
+
+    public function addRapport(RapportVeterinaire $rapport): static
+    {
+        if (!$this->rapports->contains($rapport)) {
+            $this->rapports->add($rapport);
+            $rapport->setAnimal($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRapport(RapportVeterinaire $rapport): static
+    {
+        if ($this->rapports->removeElement($rapport)) {
+            // set the owning side to null (unless already changed)
+            if ($rapport->getAnimal() === $this) {
+                $rapport->setAnimal(null);
+            }
+        }
 
         return $this;
     }
