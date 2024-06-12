dc12ea36f9a7b66b5804bc4542f41be5abc9830f
AurelienVezine <152594941+AurelienVezine@users.noreply.github.com>
2024-06-12 13:26:23 +0200

    creation entity habitat

---
+++ src/Entity/Habitat.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\HabitatRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: HabitatRepository::class)]
+class Habitat
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $commentaire_habitat = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCommentaireHabitat(): ?string
+    {
+        return $this->commentaire_habitat;
+    }
+
+    public function setCommentaireHabitat(?string $commentaire_habitat): static
+    {
+        $this->commentaire_habitat = $commentaire_habitat;
+
+        return $this;
+    }
+}
