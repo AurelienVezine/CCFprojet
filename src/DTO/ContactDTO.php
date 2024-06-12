ce489521d6569b5a2699a0dbc12b74c0f164fbd2
AurelienVezine <152594941+AurelienVezine@users.noreply.github.com>
2024-06-12 14:07:43 +0200

    creation objet contact et du formulaire

---
+++ src/DTO/ContactDTO.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\DTO;
+
+class ContactDTO
+{
+    public string $name = '';
+
+    public string $email = '';
+
+    public string $title = '';
+
+    public string $message = '';
+
+
+}
