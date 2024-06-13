a675e7912d3a02947bb6c32cb29802a9eeb8260b
AurelienVezine <152594941+AurelienVezine@users.noreply.github.com>
2024-06-13 15:45:19 +0200

    creation pour race -boucle pour afficher la liste des race -creation de create,edit,delete (non fonctionnel)

--- src/Controller/Admin/RaceController.php
+++ src/Controller/Admin/RaceController.php
@@ -2,17 +2,68 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Race;
+use App\Form\RaceType;
+use App\Repository\RaceRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
 
+#[Route("/admin/race", name: "admin.race.")]
 class RaceController extends AbstractController
 {
-    #[Route('/race', name: 'app_race')]
-    public function index(): Response
+    #[Route(name: 'index')]
+    public function index(RaceRepository $raceRepository): Response
     {
-        return $this->render('race/index.html.twig', [
-            'controller_name' => 'RaceController',
+        $races = $raceRepository->findAll();
+        return $this->render('admin/race/index.html.twig', [
+            '$races' => $races,
         ]);
     }
+
+    #[Route('/create', name: 'create')]
+    public function create(Request $request, EntityManagerInterface $em)
+    {
+        $race = new Race();
+        $form = $this->createForm(RaceType::class, $race);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($race);
+            $em->flush();
+            $this->addFlash('success', 'Votre race est bien créer');
+            return $this->redirectToRoute('admin.race.index');
+        }
+        return $this->render('admin/race/create.html.twig', [
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/{id}', name: 'edit', methods: ['GET', 'POST'], requirements: ['id' => Requirement::DIGITS])]
+    public function edit(Race $race, Request $request, EntityManagerInterface $em)
+    {
+        $form = $this->createForm(RaceType::class, $race);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            $this->addFlash('success', 'Votre race est bien modifier');
+            return $this->redirectToRoute('admin.race.index');
+        }
+        return $this->render('admin/race/edit.html.twig', [
+            'race' => $race,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'delete', methods: ['DELETE'],requirements: ['id' => Requirement::DIGITS])]
+    public function delete(Race $race, Request $request, EntityManagerInterface $em)
+    {
+        $em->remove($race);
+        $em->flush();
+        $this->addFlash('success', 'Votre race est bien supprimer');
+        return $this->redirectToRoute('admin.race.index');
+    }
+
 }
