11890e0e452b3075529efcb78b516539d1c4c159
AurelienVezine <152594941+AurelienVezine@users.noreply.github.com>
2024-06-13 10:53:42 +0200

    creation des different formulaire et controller associer plus page twig pour les formulaire

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -5,10 +5,10 @@ namespace App\Controller\Admin;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-#[route("/admin", name: "admin.")]
+
 class AdminController extends AbstractController
 {
-    #[Route('/', name: 'index')]
+    #[Route('/admin', name: 'admin.index')]
     public function index(): Response
     {
         return $this->render('admin/admin.html.twig');
