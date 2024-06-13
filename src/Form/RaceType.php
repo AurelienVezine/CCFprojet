b0d7a52b8a110f7d45f717b919504be5a6bd9f65
AurelienVezine <152594941+AurelienVezine@users.noreply.github.com>
2024-06-13 15:56:42 +0200

    creation pour race -boucle pour afficher la liste des race -creation de create,edit,delete (non fonctionnel)

--- src/Form/RaceType.php
+++ src/Form/RaceType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Race;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +13,10 @@ class RaceType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('label');
+            ->add('label')
+            ->add('save', SubmitType::class, [
+                'label' => 'Enregistrer',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
