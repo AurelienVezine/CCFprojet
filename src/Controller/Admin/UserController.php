db5be247345d41c5cb0aefd695704afca715bc2e
AurelienVezine <152594941+AurelienVezine@users.noreply.github.com>
2024-06-14 23:32:56 +0200

    creation page pour la creation d'un utilisateur

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -2,17 +2,68 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
 
+#[Route("/admin/user", name: "admin.user.")]
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    #[Route(name: 'index')]
+    public function index(UserRepository $userRepository): Response
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+        $users = $userRepository->findAll();
+        return $this->render('admin/user/index.html.twig', [
+            'users' => $users,
         ]);
     }
+
+    #[Route('/create', name: 'create')]
+    public function create(Request $request, EntityManagerInterface $em)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($user);
+            $em->flush();
+            $this->addFlash('success', 'Votre utilisateur est bien créer');
+            return $this->redirectToRoute('admin.user.index');
+        }
+        return $this->render('admin/user/create.html.twig', [
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/{id}', name: 'edit', methods: ['GET', 'POST'], requirements: ['id' => Requirement::DIGITS])]
+    public function edit(UserRepository $userRepository, Request $request, EntityManagerInterface $em)
+    {
+        $form = $this->createForm(UserType::class, $userRepository);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            $this->addFlash('success', 'Votre utilisateur est bien modifier');
+            return $this->redirectToRoute('admin.user.index');
+        }
+        return $this->render('admin/user/edit.html.twig', [
+            'users' => $userRepository,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'delete', methods: ['DELETE'],requirements: ['id' => Requirement::DIGITS])]
+    public function delete(UserRepository $userRepository, Request $request, EntityManagerInterface $em)
+    {
+        $em->remove($userRepository);
+        $em->flush();
+        $this->addFlash('success', 'Votre utilisateur est bien supprimer');
+        return $this->redirectToRoute('admin.user.index');
+    }
+
 }
