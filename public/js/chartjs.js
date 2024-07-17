4101e2b2c3438e16a8de563b79bf11658b73d47c
AurelienVezine <152594941+AurelienVezine@users.noreply.github.com>
2024-07-18 00:38:14 +0200

    resolution problem graphique

---
+++ public/js/chartjs.js
@@ -0,0 +1,34 @@
+var ctx = document.getElementById('animalViewsChart');
+
+var chart = new Chart(ctx.getContext('2d'), {
+    type: 'bar',
+    data: {
+        labels: JSON.parse(ctx.dataset.labels),
+        datasets: [{
+            label: 'Nombre de vues',
+            data: JSON.parse(ctx.dataset.values),
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 205, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+            ],
+            borderColor: [
+                'rgb(255, 99, 132)',
+                'rgb(255, 159, 64)',
+                'rgb(255, 205, 86)',
+                'rgb(75, 192, 192)',
+                'rgb(54, 162, 235)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
